a42d51537557d0fe2524d6a5c120c3841b2f58d6
Christophe baert <chrisn10@outlook.be>
2022-05-25 19:03:26 +0200

    Add project files.

---
+++ TeamOpdracht/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TeamOpdracht
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
